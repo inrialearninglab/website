1a8c03455443ad6586d91674f1d13e2666e04c9b
VIAUD Nathan <nathan.viaud@inria.fr>
2026-03-26 12:11:45 +0100

    Add tooltip to status badge

--- app/components/mooc/Status.vue
+++ app/components/mooc/Status.vue
@@ -10,14 +10,26 @@ const { t } = useI18n({
 });
 
 const statusInfo: Record<(typeof statuses)[number], BadgeProps["color"]> = {
-    open: "success",
-    closed: "error",
-    archived: "warning",
+    open: {
+        color: "success",
+    },
+    closed: {
+        tooltip: t("closed-callout"),
+        icon: "lucide:circle-alert",
+        color: "error",
+    },
+    archived: {
+        tooltip: t("archived-callout"),
+        icon: "lucide:circle-alert",
+        color: "warning",
+    },
 };
 </script>
 
 <template>
-    <UBadge variant="subtle" :color="statusInfo[status]" :label="t(status)" />
+    <UTooltip :text="statusInfo[status]?.tooltip" :delay-duration="0">
+        <UBadge variant="subtle" :color="statusInfo[status].color" :label="t(status)" :icon="statusInfo[status].icon" />
+    </UTooltip>
 </template>
 
 <i18n lang="json">
@@ -25,12 +37,16 @@ const statusInfo: Record<(typeof statuses)[number], BadgeProps["color"]> = {
     "fr": {
         "open": "Ouvert",
         "closed": "Fermé",
-        "archived": "Archivé"
+        "archived": "Archivé",
+        "closed-callout": "Ce cours est fermé sur FUN et n'est plus accessible",
+        "archived-callout": "Ce cours est archivé, son accès est limité en lecture seule"
     },
     "en": {
         "open": "Open",
         "closed": "Closed",
-        "archived": "Archived"
+        "archived": "Archived",
+        "closed-callout": "This course is closed on FUN and no longer accessible",
+        "archived-callout": "This course is archived, access is limited to read-only"
     }
 }
 </i18n>
