f11d42b91e669c2037f07c610972942a2ec07ef8
VIAUD Nathan <nathan.viaud@inria.fr>
2026-03-30 14:00:22 +0200

    fix: iframe css

--- app/components/content/Publications.vue
+++ app/components/content/Publications.vue
@@ -4,6 +4,6 @@
     <iframe
         class="w-full"
         style="height: 6230px"
-        src="https://haltools.inria.fr/Public/afficheRequetePubli.php?struct=476527+%3B+444860+&CB_auteur=oui&CB_titre=oui&CB_article=oui&CB_vignette=oui&langue=Francais&tri_exp=annee_publi&tri_exp2=typdoc&tri_exp3=date_publi&ordre_aff=TA&Fen=Aff&css=http://localhost:3000/hal.css"
+        src="https://haltools.inria.fr/Public/afficheRequetePubli.php?struct=476527+%3B+444860+&CB_auteur=oui&CB_titre=oui&CB_article=oui&CB_vignette=oui&langue=Francais&tri_exp=annee_publi&tri_exp2=typdoc&tri_exp3=date_publi&ordre_aff=TA&Fen=Aff&css=https://mooc-siteweb.inria.fr/hal.css"
     />
 </template>
