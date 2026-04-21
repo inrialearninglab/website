b537056c191ef971aaf949d42893b6c5243b64ac
VIAUD Nathan <nathan.viaud@inria.fr>
2026-04-21 11:08:12 +0200

    ajout page 404

---
+++ app/pages/404.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const { t } = useI18n();
+</script>
+
+<template>
+    <UError
+        :error="{
+            statusCode: 404,
+            statusMessage: t('error-title'),
+            message: t('error-message'),
+        }"
+    />
+</template>
+
+<i18n lang="json" scoped>
+{
+    "fr": {
+        "error-title": "Page introuvable",
+        "error-message": "La page que vous cherchez n'existe pas"
+    },
+    "en": {
+        "error-title": "Page not found",
+        "error-message": "The page you are looking for does not exist"
+    }
+}
+</i18n>
